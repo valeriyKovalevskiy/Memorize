5cf5a78a7d8007aac7d115650974fc4e58a595f1
v.kovalevskiy <v.kovalevskiy@vironit.co.uk>
2020-06-02 18:04:19 +0300

    Update ContentView.swift
    The end of lecture one

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,9 +10,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4){ index in
+                CardView(isFaceUp: false)
+            }
+        }
+            
+        .foregroundColor(Color.orange)
+        .padding(50)
+        .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill().foregroundColor(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
